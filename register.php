365edbd71e40929434a3a7ba52902148c1456474
nitn19 <85925451+nitn19@users.noreply.github.com>
2023-08-11 11:39:56 +0530

    Done

---
+++ register.php
@@ -0,0 +1,20 @@
+<?php
+// This is a simplified example and lacks proper security measures
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    // Perform database operations to validate user credentials (not shown here)
+
+    // Assuming the credentials are valid
+    if ($username === "nitin" && $password === "nitin") {
+        // Redirect to the subscription plans page after successful login
+        header("Location: subscription_plans.html");
+        exit();
+    } else {
+        // Display an error message
+        $error_message = "Invalid username or password.";
+    }
+}
+?>
